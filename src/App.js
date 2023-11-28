72e430eb9a0842e1db842887cc92f5543264f8d5
vinod kumar <av859669@gmail.com>
2023-11-28 13:44:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1 className="bg-black text-white ">this is heading</h1>
       <div className='flex-col flex-wrap'>
-      <div className='mb-30'><label  htmlFor="name">Name:</label></div>
+      <div className='mb-30'><label  htmlFor="name">Name of candidate:</label></div>
       <input className="placeholder:italic placeholder:text-slate-400  bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" placeholder='yourname' id="name" />
         </div>
 
